85ee4a9631d003e05d8845b34617aeeb34e76ed0
wlwannpan <wlwanpan@uwaterloo.ca>
2017-04-11 11:23:25 -0700

    added filter and sort

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -47,4 +47,39 @@ describe('TodoAPI', () => {
             expect(actualTodos).toEqual(todos);
         });
     });
+    describe('filterTodos', () => {
+        var todos = [{
+            id: 1,
+            text: 'Some text',
+            completed: true
+        }, {
+            id: 2,
+            text: 'Other text',
+            completed: false
+        }, {
+            id: 3,
+            text: 'Some text2',
+            completed: true
+        }];
+        it('should return all items is showCompleted is true', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, "");
+            expect(filteredTodos.length).toBe(3);
+        });
+        it('should return non-completed todos is showCompleted is false', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, false, "");
+            expect(filteredTodos.length).toBe(1);
+        });
+        it('should sort by completed status', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, "");
+            expect(filteredTodos[0].completed).toBe(false);
+        });
+        it('should filter todos by searchText', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, "some");
+            expect(filteredTodos.length).toBe(2);
+        });
+        it('should return all todos if searchText is empty', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, "");
+            expect(filteredTodos.length).toBe(3);
+        });
+    });
 });
