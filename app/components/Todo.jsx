82504cd209f93a33770c7fb10514c47f37293f60
wlwannpan <wlwanpan@uwaterloo.ca>
2017-05-03 14:52:40 -0700

    added actions

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -10,7 +10,7 @@ var Todo = React.createClass({
             var timestamp = createdAt;
 
             if (completed) {
-                message = 'Completed';
+                message = 'Completed: ';
                 timestamp = completedAt;
             }
 
